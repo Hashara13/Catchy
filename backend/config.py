3bdf2333ac3d1583fa9362378d6500465f84c20b
Hashara13 <hnethmin13@gmail.com>
2024-09-21 12:09:40 +0530

    minor changes

---
+++ backend/config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///applications.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Flask-Mail configuration
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
