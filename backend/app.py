739208465a9130715748e6be0cb0189d562acdc6
Hashara13 <hnethmin13@gmail.com>
2024-10-04 20:15:43 +0530

    minor changes

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,8 @@ CORS(app)
 # app.config['MAIL_SERVER'] = 'smtp.example.com'
 # app.config['MAIL_PORT'] = 587
 # app.config['MAIL_USERNAME'] = 'your_email@example.com'
-# app.config['MAIL_PASSWORD'] = 'your_password'
-# app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PASSWORD'] = 'your_password'
+app.config['MAIL_USE_TLS'] = True
 mail = Mail(app)
 db = SQLAlchemy(app)
 
