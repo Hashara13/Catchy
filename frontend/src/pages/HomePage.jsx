cd371be03631a2d7163485417bca808b6db42235
Hashara13 <hnethmin13@gmail.com>
2024-10-09 12:55:35 +0530

    Home UI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 const HomePage = () => {
+  const[cv,setCV]=useState("");
+  const[jd,setJD]=useState("");
+  const[track,setTrack]=useState(null);
+
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    const response = await axios.post('http://127.0.0.1:5000/match',{
+      cv:cv,
+      jd:jd,
+    })
+    setTrack(response.data.track)
+  }
   return (
     <div>HomePage</div>
   )
